1ed28e5c53efe333352b0b9a4edf379302164005
priyaaansh13 <46779649+priyaaansh13@users.noreply.github.com>
2020-12-10 09:58:24 +0530

    starting student section

---
+++ src/components/custom/MCQ.js
@@ -0,0 +1,88 @@
+import React from "react"
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardBody,
+  FormGroup,
+  Row,
+  Col,
+  Input,
+  Form,
+  Button,
+  Label
+} from "reactstrap"
+
+import Checkbox from "../@vuexy/checkbox/CheckboxesVuexy"
+import Radio from "../@vuexy/radio/RadioVuexy"
+
+import { Check } from "react-feather"
+
+class MCQ extends React.Component {
+  render() {
+    return (
+      <Card>
+        <CardBody>
+          <Form>
+            <Row>
+            <Col sm="12">
+                <div>question?</div>
+                </Col>
+                <Col sm="12">
+                <Radio
+            label="Default"
+            color="primary"
+            defaultChecked={false}
+            name="Option1"
+            className="py-50"
+          />
+           <Radio
+            label="Default"
+            color="primary"
+            defaultChecked={false}
+            name="Option2"
+            className="py-50"
+          />
+           <Radio
+            label="Default"
+            color="primary"
+            defaultChecked={false}
+            name="Option3"
+            className="py-50"
+          />
+           <Radio
+            label="Default"
+            color="primary"
+            defaultChecked={false}
+            name="Option4"
+            className="py-50"
+          />
+                </Col>
+              <Col sm="12">
+                <FormGroup>
+                  <Button.Ripple
+                    color="primary"
+                    type="submit"
+                    className="mr-1 mb-1"
+                    onClick={e => e.preventDefault()}
+                  >
+                    Next
+                  </Button.Ripple>
+                  <Button.Ripple
+                    outline
+                    color="warning"
+                    type="reset"
+                    className="mb-1"
+                  >
+                   Clear
+                  </Button.Ripple>
+                </FormGroup>
+              </Col>
+            </Row>
+          </Form>
+        </CardBody>
+      </Card>
+    )
+  }
+}
+export default MCQ;
---
+++ src/sections/Student/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Card, CardBody, Row, Col, Button } from "reactstrap";
+import MCQ from "../../components/custom/MCQ"
+
+class StudentSection extends React.Component{
+    render(){
+        return (
+          <MCQ/>
+        )
+    }
+}
+
+
+export default StudentSection;
