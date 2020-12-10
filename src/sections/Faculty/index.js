23fe31fb4ab86dbff2dca4594663f3454f7bc583
priyaaansh13 <46779649+priyaaansh13@users.noreply.github.com>
2020-12-10 12:17:13 +0530

    clearing unnecessary code and starting faculty section

---
+++ src/sections/Faculty/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Dash from "./papermaking/SubjectDash"
+
+class FacultySection extends React.Component{
+    render(){
+        return (
+         <Dash/>
+        )
+    }
+}
+
+
+export default FacultySection;
